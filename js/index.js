6573d57de69d21cd794ba3a22037d43943a10ccc
Marco Whyte <mwhyte@umass.edu>
2020-01-14 17:26:31 -0600

    finished propagation and prevent default

--- js/index.js
+++ js/index.js
@@ -1,57 +1,72 @@
 // Your code goes here
 const imagehover = document.querySelectorAll(".img-content").forEach(element => {
-    element.addEventListener("mouseover", () =>{
-        element.style.transform="scale(1.2)";
-        element.style.transition="transform 0.3s";
-    })
-    element.addEventListener("mouseleave", () =>{
-        element.style.transform="scale(1)";
-    })
+element.addEventListener("mouseover", () =>{
+    element.style.transform="scale(1.2)";
+    element.style.transition="transform 0.3s";
+})
+element.addEventListener("mouseleave", () =>{
+    element.style.transform="scale(1)";
+})
 })
 
 const h2 = document.querySelectorAll("h2").forEach(element => {
-    element.addEventListener("dblclick", () => {
-        element.style.backgroundColor= "black";
-        element.style.color= "#EDC9AF";
-        element.style.transform ="scale(1.2)";
-        element.style.transition="transform 0.3s";
-    })
-    element.addEventListener("mouseleave", () => {
-        element.style.backgroundColor= "lavender";
-        element.style.color= "black";
-        element.style.transform ="scale(1.0)";
-    })
+element.addEventListener("dblclick", () => {
+    element.style.backgroundColor= "black";
+    element.style.color= "#EDC9AF";
+    element.style.transform ="scale(1.2)";
+    element.style.transition="transform 0.3s";
+})
+element.addEventListener("mouseleave", () => {
+    element.style.backgroundColor= "lavender";
+    element.style.color= "black";
+    element.style.transform ="scale(1.0)";
+})
 })
 
 const h4 = document.querySelectorAll('h4').forEach(element => {
-    element.addEventListener("click", () => {
-        element.style.textAlign = "center"
-        element.style.zoom = "1.7"
-    })
+element.addEventListener("click", () => {
+    element.style.textAlign = "center"
+    element.style.zoom = "1.7"
+})
 })
 
 const imageResize = document.querySelector('.content-destination img');
-    window.addEventListener('resize', () => {
-        imageResize.src = 'https://images.unsplash.com/photo-1578926288207-a90a5366759d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2022&q=80'
-    })
-    imageResize.addEventListener('mousedown', (event) => {
-        event.target.style.opacity = '0';
-    })
-    imageResize.addEventListener('dblclick', (event) => {
-        event.target.style.opacity = '.50';
-    })
-    window.addEventListener("load", function(event) {
-        alert("Welcome to the Fun Bus!");
-        });
+window.addEventListener('resize', () => {
+    imageResize.src = 'https://images.unsplash.com/photo-1578926288207-a90a5366759d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2022&q=80'
+})
+imageResize.addEventListener('mousedown', (event) => {
+    event.target.style.opacity = '0';
+    
+})
+imageResize.addEventListener('dblclick', (event) => {
+    event.target.style.opacity = '.50';
+})
+window.addEventListener("load", function(event) {
+    alert("Welcome to the Fun Bus!");
+    });
 const p = document.querySelectorAll('p').forEach(element => {
-    element.addEventListener('copy', () =>{
-        alert('are you trying to plagerize?')
-    })
+element.addEventListener('copy', () =>{
+    alert('are you trying to plagerize?')
+})
 })
 const body = document.querySelector('body');
-    body.addEventListener('keydown', () => {
-        body.style.backgroundColor = "skyblue";
+body.addEventListener('keydown', () => {
+    body.style.backgroundColor = "skyblue";
+})
+body.addEventListener('keyup', () => {
+    body.style.backgroundColor = "lavender"
+})
+const header = document.querySelector('header');
+const funBus = document.querySelector('.logo-heading');
+funBus.addEventListener('click', () => {
+    funBus.style.color = 'purple';
+    event.stopPropagation();
+})
+header.addEventListener('click', () => {
+    header.style.backgroundColor = 'green';
+})
+const navItems = document.querySelectorAll('.nav-link').forEach(element => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
     })
-    body.addEventListener('keyup', () => {
-        body.style.backgroundColor = "lavender"
-    })
+})

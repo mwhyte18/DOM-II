0826ba0d266c310a3bb92bbacfd5033073cf7803
Marco Whyte <mwhyte@umass.edu>
2020-01-14 16:23:42 -0600

    finished 10 unique event listeners

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const h2 = document.querySelectorAll("h2").forEach(element => {
         element.style.transition="transform 0.3s";
     })
     element.addEventListener("mouseleave", () => {
-        element.style.backgroundColor= "white";
+        element.style.backgroundColor= "lavender";
         element.style.color= "black";
         element.style.transform ="scale(1.0)";
     })
@@ -34,6 +34,24 @@ const imageResize = document.querySelector('.content-destination img');
     window.addEventListener('resize', () => {
         imageResize.src = 'https://images.unsplash.com/photo-1578926288207-a90a5366759d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2022&q=80'
     })
+    imageResize.addEventListener('mousedown', (event) => {
+        event.target.style.opacity = '0';
+    })
+    imageResize.addEventListener('dblclick', (event) => {
+        event.target.style.opacity = '.50';
+    })
     window.addEventListener("load", function(event) {
         alert("Welcome to the Fun Bus!");
         });
+const p = document.querySelectorAll('p').forEach(element => {
+    element.addEventListener('copy', () =>{
+        alert('are you trying to plagerize?')
+    })
+})
+const body = document.querySelector('body');
+    body.addEventListener('keydown', () => {
+        body.style.backgroundColor = "skyblue";
+    })
+    body.addEventListener('keyup', () => {
+        body.style.backgroundColor = "lavender"
+    })
